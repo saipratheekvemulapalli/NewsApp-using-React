3a698d635f5ca6169c47e32c918ba1cb34b0e572
Sai Pratheek <saipratheekvemulapalli@gmail.com>
2024-08-13 14:36:11 +0530

    update date 1.1

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 
 export default class Newsitem extends Component {
-   
-
-
   render() {
-    let { title, description, imageUrl, url ,author, date} = this.props;
+    const { title, description, imageUrl, url, author, date } = this.props;
+
     return (
-     
-       <div  className='my-3'>
-            
+      <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={!imageUrl?"https://imageio.forbes.com/specials-images/imageserve/66b6a91856b13b39ded769ed/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds":imageUrl} className="card-img-top" alt="/" />
+          <img
+            src={
+              !imageUrl
+                ? "https://imageio.forbes.com/specials-images/imageserve/66b6a91856b13b39ded769ed/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="News"
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p className="card-text"><small className="text-muted">By {!author?"Unknown source":author} last updated on {date}</small></p>
-            <a  rel="noreferer" href={url} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown source" : author} last updated on {date}
+              </small>
+            </p>
+            <a
+              rel="noopener noreferrer"
+              href={url}
+              target='_blank'
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
           </div>
         </div>
-      
-        </div>
+      </div>
     );
   }
 }
